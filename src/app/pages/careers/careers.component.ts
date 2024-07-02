1c1a174f88d43b4855f6209ce5d3ceeb9e0d2b51
“vishal <vishalyadav3677@gmail.com>
2024-07-02 18:46:16 +0530

    form with email js

--- src/app/pages/careers/careers.component.ts
+++ src/app/pages/careers/careers.component.ts
@@ -1,19 +1,53 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { MetaService } from '../../services/meta.service';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 @Component({
   selector: 'app-careers',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './careers.component.html',
   styleUrl: './careers.component.scss',
 })
 export class CareersComponent implements OnInit {
   private metaService = inject(MetaService);
+  contactFrom!: FormGroup;
+  loading:boolean = false;
+  constructor(public fb: FormBuilder){}
   ngOnInit(): void {
     this.metaService.updateMeta({
       slug: 'careers',
       description: 'Quantum Edge Labs | Careers',
     });
+    this.contactFrom = this.fb.group({
+      name: [null, Validators.required],
+      email: [null, Validators.required],
+      phone: [null, Validators.required],
+      position: [null, Validators.required],
+      portfolio_link: [null],
+      
+      message: [null]
+    })
   }
+
+  public sendEmail(data: Event) {
+    this.loading = true;
+    emailjs
+      .sendForm('service_w036nuv', 'template_rxl7wqr', data.target as HTMLFormElement, {
+        publicKey: 'V08l-WfCBVyrm2dLo',
+      })
+      .then(
+        (res) => {
+          this.contactFrom.reset()
+          this.loading = false;
+        },
+        (error:any) => {
+          console.log(error,"er")
+          this.loading = false;
+        },
+      );
+  }
+
+  
+
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,19 +1,48 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { MetaService } from '../../services/meta.service';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent implements OnInit {
   private metaService = inject(MetaService);
+  contactFrom!: FormGroup;
+  loading:boolean = false;
+  constructor(public fb: FormBuilder){}
   ngOnInit(): void {
     this.metaService.updateMeta({
       slug: 'contact-us',
       description: 'Quantum Edge Labs | Contact Us',
     });
+    this.contactFrom = this.fb.group({
+      name: [null, Validators.required],
+      email: [null, Validators.required],
+      phone: [null, Validators.required],
+      services: [null, Validators.required],
+      message: [null]
+    })
+  }
+
+  public sendEmail(data: Event) {
+    this.loading = true;
+    emailjs
+      .sendForm('service_w036nuv', 'template_11235ym', data.target as HTMLFormElement, {
+        publicKey: 'V08l-WfCBVyrm2dLo',
+      })
+      .then(
+        (res) => {
+          this.contactFrom.reset()
+          this.loading = false;
+        },
+        (error:any) => {
+          console.log(error,"er")
+          this.loading = false;
+        },
+      );
   }
 }
