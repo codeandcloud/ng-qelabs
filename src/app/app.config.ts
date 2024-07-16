43278da7f320917e8cc31834a962fda365156827
“vishal <vishalyadav3677@gmail.com>
2024-07-16 11:11:09 +0530

    firebase added in config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,7 @@ import { RouterModule, TitleStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
 import { TemplatePageTitleStrategy } from './services/title.strategy';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +17,19 @@ export const appConfig: ApplicationConfig = {
       RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
       BrowserAnimationsModule
     ),
+
+     importProvidersFrom(provideFirebaseApp(() => initializeApp(
+      {
+        apiKey: "AIzaSyDer-3nEgkd8ywxHfAfcnT6Sd65eAmMBEY",
+        authDomain: "quantum-edge-labs.firebaseapp.com",
+        projectId: "quantum-edge-labs",
+        storageBucket: "quantum-edge-labs.appspot.com",
+        messagingSenderId: "1082574169432",
+        appId: "1:1082574169432:web:a73d3226ba2fc988fdf12d",
+        measurementId: "G-EFHE08ERRQ"
+      }
+      ))), 
+
     {
       provide: TitleStrategy,
       useClass: TemplatePageTitleStrategy,
