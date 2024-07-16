4b0b958f2c6b3d603e28ae4ed5858edacbae9481
Naveen Jose <naveen.chakiath@gmail.com>
2024-07-16 11:25:06 +0530

    fix: firebase options to provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,30 +6,31 @@ import {
 import { provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, TitleStrategy } from '@angular/router';
+import {
+  FirebaseOptions,
+  initializeApp,
+  provideFirebaseApp,
+} from '@angular/fire/app';
 
 import { routes } from './app.routes';
 import { TemplatePageTitleStrategy } from './services/title.strategy';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
+const firebaseOptions: FirebaseOptions = {
+  apiKey: 'AIzaSyDer-3nEgkd8ywxHfAfcnT6Sd65eAmMBEY',
+  authDomain: 'quantum-edge-labs.firebaseapp.com',
+  projectId: 'quantum-edge-labs',
+  storageBucket: 'quantum-edge-labs.appspot.com',
+  messagingSenderId: '1082574169432',
+  appId: '1:1082574169432:web:a73d3226ba2fc988fdf12d',
+  measurementId: 'G-EFHE08ERRQ',
+};
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
       RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
       BrowserAnimationsModule
     ),
-
-     importProvidersFrom(provideFirebaseApp(() => initializeApp(
-      {
-        apiKey: "AIzaSyDer-3nEgkd8ywxHfAfcnT6Sd65eAmMBEY",
-        authDomain: "quantum-edge-labs.firebaseapp.com",
-        projectId: "quantum-edge-labs",
-        storageBucket: "quantum-edge-labs.appspot.com",
-        messagingSenderId: "1082574169432",
-        appId: "1:1082574169432:web:a73d3226ba2fc988fdf12d",
-        measurementId: "G-EFHE08ERRQ"
-      }
-      ))), 
-
+    provideFirebaseApp(() => initializeApp(firebaseOptions)),
     {
       provide: TitleStrategy,
       useClass: TemplatePageTitleStrategy,
