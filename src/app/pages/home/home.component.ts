3f7c120c228639a1eee734a9a871377e324de578
marykutty <jibythomas07373@gmail.com>
2024-07-26 20:58:34 +0530

    home without animation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { MetaService } from '../../services/meta.service';
-import { OpenRolesComponent } from "../../components/open-roles/open-roles.component";
-
-
+import { Banner } from '../../models/banner.type';
+import { PageBannerComponent } from '../../components/page-banner/page-banner.component';
+import { QeButtonComponent } from '../../components/qe-button/qe-button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [PageBannerComponent, QeButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -15,7 +15,9 @@ export class HomeComponent implements OnInit {
   private metaService = inject(MetaService);
 
   file?: File;
-
+  banner!: Banner;
+  buttonText = 'View Project';
+  isButtonDisabled!: false;
 
   ngOnInit(): void {
     this.metaService.updateMeta({
@@ -23,4 +25,15 @@ export class HomeComponent implements OnInit {
       description: `Quantum Edge Labs is more than an IT solutions provider, we're the innovation architects shaping tech around your unique needs. We don't host our own party, we invite you to be the centrepiece. It's your business, your vision — and our solutions start and end with you.`,
     });
   }
+  handleButtonClick = () => {
+    console.log('data');
+  };
+  setBanner = () => {
+    const subText = `Whether you're looking for information, have questions, or want to explore partnership opportunities, don't hesitate to reach out to us. Your inquiries are important to us, and we're here to assist you.`;
+    this.banner = {
+      title: 'Contact Us',
+      description: `<p>We're excited to connect with you!</p><br><p>${subText}</p>`,
+      image: 'images/contact/contact.png',
+    };
+  };
 }
